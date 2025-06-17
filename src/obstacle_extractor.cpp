6cdad6753ce873b4230b10777b84fc5646eb52bd
johook <gns453@kookmin.ac.kr>
2025-06-17 10:47:10 +0000

    Debug Mode

--- src/obstacle_extractor.cpp
+++ src/obstacle_extractor.cpp
@@ -550,10 +550,10 @@ void ObstacleExtractor::transformObstacles() {
         m_transform = tf_buffer_->lookupTransform(p_frame_id_, base_frame_id_, tf2::TimePointZero);
     } 
     catch (const tf2::TransformException & ex) {
-        RCLCPP_INFO(
-        nh_->get_logger(), "Could not transformmm %s to %s: %s",
-        p_frame_id_.c_str(), base_frame_id_.c_str(), ex.what());
-        return;
+        // RCLCPP_INFO(
+        // nh_->get_logger(), "Could not transformmm %s to %s: %s",
+        // p_frame_id_.c_str(), base_frame_id_.c_str(), ex.what());
+        // return;
     }
 
     for (Segment& s : segments_) {
